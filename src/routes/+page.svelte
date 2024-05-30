4cfb544cdf6197f9423d49732c3475cb8efe4920
Vy Vu <74190308+bellavee@users.noreply.github.com>
2024-05-30 16:23:43 +0200

    update header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,36 +5,14 @@
 	import Footer from './pages/Footer.svelte';
 	import About from './pages/About.svelte';
 	import Hobbies from './pages/Hobbies.svelte';
-	import img_background from '$lib/assets/bg_xl.jpg'
+	import Header from './pages/Header.svelte';
 </script>
-
-<style>
-  .aspect-ratio {
-    position: relative;
-    width: 100%; /* Adjust width as needed */
-    padding-top: 30%; /* 100 * 9 / 21 */
-    overflow: hidden;
-	margin-bottom: 120px;
-  }
-
-  .aspect-ratio img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-</style>
 
 <!--<Navigation/>-->
 
 <div class="bg-navy-900">
 	<div class="bg-navy-800 max-w-6xl mx-auto">
-
-		<div class="aspect-ratio">
-			<img src="{img_background}" alt="Background" />
-		</div>
+		<Header/>
 
 		<div class="px-20">
 			<!-- <Presentation/> -->
---
+++ src/routes/pages/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+    import img_background from '$lib/assets/bg_xl.jpg'
+</script>
+
+<style>
+    .aspect-ratio {
+      position: relative;
+      width: 100%; /* Adjust width as needed */
+      padding-top: 30%; /* 100 * 9 / 21 */
+      overflow: hidden;
+      margin-bottom: 120px;
+    }
+  
+    .aspect-ratio img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  </style>
+
+<div class="aspect-ratio">
+    <img src="{img_background}" alt="Background" />
+</div>
