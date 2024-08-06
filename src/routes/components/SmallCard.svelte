5d49a0dd2fe416654bba59b87182c1965e490d1a
Vy Vu <74190308+bellavee@users.noreply.github.com>
2024-08-06 17:49:38 +0200

    update cv

--- src/routes/components/SmallCard.svelte
+++ src/routes/components/SmallCard.svelte
@@ -6,7 +6,7 @@
 <div class="w-full">
 	<div class="grid p-5 relative bg-white dark:bg-navy-700 mx-auto border-page-500 border dark:border-0">
 
-		<a href={fileData.link} class="font-bold text-xl lg:text-xl hover:underline">{fileData.title}</a>
+		<a href={fileData.link} target="_blank" class="font-bold text-xl lg:text-xl hover:underline">{fileData.title}</a>
 		<div class="whitespace-pre-wrap">
 			<span class="text-sm">{fileData.description}</span>
 		</div>
