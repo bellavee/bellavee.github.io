da15a93ed24a7f55bc01b334f5d058bb9c925a9d
Vy Vu <74190308+bellavee@users.noreply.github.com>
2024-10-28 17:50:38 +0100

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const config = {
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
+		prerender: {
+			entries: ["/", "/bella_sekai", "/flashcard", "/schedule_emloyees", "/twitter_alarm"]
 		}
 	}
 };
