6676cfea3a3b1058667281440076cd44ea86fc08
Vy Vu <74190308+bellavee@users.noreply.github.com>
2024-10-28 18:42:42 +0100

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 ﻿<script>
     import '../app.css';
-    import { navigating } from '$app/stores';
+    // import { navigating } from '$app/stores';
     import { fly } from 'svelte/transition';
     import Footer from "../lib/components/Footer.svelte";
 </script>
 
 <div class="flex flex-col min-h-screen bg-navy-900">
-    {#key $navigating}
+    <!--{#key $navigating}-->
         <main class="flex-grow" in:fly={{ y: 50, duration: 400, delay: 300 }}>
             <slot />
         </main>
-    {/key}
+    <!--{/key}-->
     <Footer />
 </div>
